89db1398008a44dd3505aa1b96e154e47050119b
Gabriel Martins Paz <th3gabrielmartins@gmail.com>
2026-06-16 00:50:18 -0300

    Adicionar modelo de habilidades e repositório para gerenciar dados de habilidades.

---
+++ src/main/java/com/gabriel/empregos/core/repositories/SkillRepository.java
@@ -0,0 +1,10 @@
+package com.gabriel.empregos.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabriel.empregos.core.models.skills;
+
+public interface SkillRepository extends JpaRepository<skills, Long> {
+
+
+}
