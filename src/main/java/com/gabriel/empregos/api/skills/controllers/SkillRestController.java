58af73d431b4889b13bb72f8d8471f36e9702ea5
Gabriel Martins Paz <th3gabrielmartins@gmail.com>
2026-06-18 22:50:44 -0300

    Adicionar controlador SkillRestController para gerenciar requisições de habilidades

---
+++ src/main/java/com/gabriel/empregos/api/skills/controllers/SkillRestController.java
@@ -0,0 +1,32 @@
+package com.gabriel.empregos.api.skills.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabriel.empregos.api.skills.dtos.SkillResponse;
+import com.gabriel.empregos.api.skills.mappers.SkillMapper;
+import com.gabriel.empregos.core.repositories.SkillRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/skills")
+public class SkillRestController {
+
+    private final SkillMapper skillMapper;
+    private final SkillRepository skillRepository;
+
+    @GetMapping
+    public List<SkillResponse> findAll() {
+        return skillRepository.findAll()
+                .stream()
+                .map(skillMapper::toSkillResponse)
+                .toList();
+        
+    }
+
+}
