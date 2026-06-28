047a7f3dbaf922b045a83b8e455929d8bc101cfd
Gabriel Martins Paz <th3gabrielmartins@gmail.com>
2026-06-28 00:41:47 -0300

    Adicionar método create no JobRestController para criar novos empregos

--- src/main/java/com/gabriel/empregos/api/jobs/controllers/JobRestController.java
+++ src/main/java/com/gabriel/empregos/api/jobs/controllers/JobRestController.java
@@ -2,17 +2,24 @@ package com.gabriel.empregos.api.jobs.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gabriel.empregos.api.jobs.dtos.JobRequest;
 import com.gabriel.empregos.api.jobs.dtos.JobResponse;
 import com.gabriel.empregos.api.jobs.mappers.JobMapper;
 import com.gabriel.empregos.core.exceptions.JobNotFoundException;
 import com.gabriel.empregos.core.repositories.JobRepository;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -41,4 +48,11 @@ public class JobRestController {
         return jobMapper.toJobResponse(job);
     }
 
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public JobResponse create(@RequestBody @Valid JobRequest jobRequest) {
+        var job = jobMapper.toJob(jobRequest);
+        job = jobRepository.save(job);
+        return jobMapper.toJobResponse(job);
+    }
 }
