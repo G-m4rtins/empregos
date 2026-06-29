e8fffe90d0c522f74c14f10db0b51240f2e95667
Gabriel Martins Paz <th3gabrielmartins@gmail.com>
2026-06-29 01:38:10 -0300

    Adicionar suporte a HATEOAS com SkillAssembler e atualizar SkillResponse para herdar de RepresentationModel

--- src/main/java/com/gabriel/empregos/api/skills/controllers/SkillRestController.java
+++ src/main/java/com/gabriel/empregos/api/skills/controllers/SkillRestController.java
@@ -1,8 +1,8 @@
 package com.gabriel.empregos.api.skills.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.BeanUtils;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.ResponseEntity;
+
+import com.gabriel.empregos.api.skills.assemblers.SkillAssembler;
 import com.gabriel.empregos.api.skills.dtos.SkillRequest;
 import com.gabriel.empregos.api.skills.dtos.SkillResponse;
 import com.gabriel.empregos.api.skills.mappers.SkillMapper;
@@ -29,50 +32,60 @@ public class SkillRestController {
 
     private final SkillMapper skillMapper;
     private final SkillRepository skillRepository;
+    private final SkillAssembler skillassembler;
 
     @GetMapping
-    public List<SkillResponse> findAll() {
-        return skillRepository.findAll()
+    public CollectionModel<EntityModel<SkillResponse>> findAll() {
+        var skills = skillRepository.findAll()
                 .stream()
-                .map(skill -> skillMapper.toSkillResponse(skill))
+                .map(skillMapper::toSkillResponse)
                 .toList();
-        
+
+        return skillassembler.toCollectionModel(skills);
     }
 
     @GetMapping("/{id}")
-    public SkillResponse findById(@PathVariable Long id) {
-        return skillRepository.findById(id).map(skill -> skillMapper.toSkillResponse(skill))
+    public EntityModel<SkillResponse> findById(@PathVariable Long id) {
+        var skill = skillRepository.findById(id).map(skillMapper::toSkillResponse)
                 .orElseThrow(SkillNotFoundException::new);
+
+        return skillassembler.toModel(skill);
 
     }
 
     @PostMapping
     @ResponseStatus(org.springframework.http.HttpStatus.CREATED)
-    public SkillResponse create(@Valid @RequestBody SkillRequest skillRequest) {
+    public EntityModel<SkillResponse> create(@Valid @RequestBody SkillRequest skillRequest) {
         var skill = skillMapper.toSkill(skillRequest);
         var savedSkill = skillRepository.save(skill);
-        return skillMapper.toSkillResponse(savedSkill);
+        var skillResponse = skillMapper.toSkillResponse(savedSkill);
+
+        return skillassembler.toModel(skillResponse);
     }
 
     @PutMapping("/{id}")
-    public SkillResponse update(@PathVariable Long id, @Valid @RequestBody SkillRequest skillRequest) {
+    public EntityModel<SkillResponse> update(@PathVariable Long id, @Valid @RequestBody SkillRequest skillRequest) {
         var existingSkill = skillRepository
                             .findById(id)
                             .orElseThrow(SkillNotFoundException::new);
-        
+
         BeanUtils.copyProperties(skillRequest, existingSkill, "id");
         var updatedSkill = skillRepository.save(existingSkill);
-        return skillMapper.toSkillResponse(updatedSkill);
+        var skillResponse = skillMapper.toSkillResponse(updatedSkill);
+
+        return skillassembler.toModel(skillResponse);
+
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(org.springframework.http.HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         var existingSkill = skillRepository
                             .findById(id)
                             .orElseThrow(SkillNotFoundException::new);
-        
+
         skillRepository.delete(existingSkill);
+        return ResponseEntity.noContent().build();
+
     }
 
 }
